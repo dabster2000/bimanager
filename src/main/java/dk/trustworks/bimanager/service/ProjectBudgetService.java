0f17528e4563d7ce993a29a3c57a64aa5f98d8bb
hans <hans.lassen@trustworks.dk>
2015-06-27 15:56:14 +0200

    v2.0.0beta4 Fixed bug, where current realized budget month wasn't updated correctly

--- src/main/java/dk/trustworks/bimanager/service/ProjectBudgetService.java
+++ src/main/java/dk/trustworks/bimanager/service/ProjectBudgetService.java
@@ -63,7 +63,8 @@ public class ProjectBudgetService extends DefaultLocalService {
                     for (TaskWorkerConstraint taskWorkerConstraint : restClient.getTaskWorkerConstraint(task.getUUID())) {
                         Calendar calendar = Calendar.getInstance();
                         calendar.set(year, month, 1, 0, 0);
-                        if(year < 2016 && month < 7) calendar = Calendar.getInstance();
+                        if(year < 2016 && month < 6) calendar = Calendar.getInstance();
+                        if(year >= Calendar.getInstance().get(Calendar.YEAR) && month > Calendar.getInstance().get(Calendar.MONTH))  calendar = Calendar.getInstance();
                         long specifiedTime = calendar.toInstant().toEpochMilli();
                         List<TaskWorkerConstraintBudget> budgets = restClient.getBudgetsByTaskWorkerConstraintUUIDAndMonthAndYearAndDate(taskWorkerConstraint, month, year, specifiedTime);
                         for (Work work : allWork) {
